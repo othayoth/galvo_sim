32616f87726de18c2a4b87178d8276ee7fe52f95
Ratan Othayoth <ratan@janelia.hhmi.org>
2022-03-06 08:25:53 -0500

    cleaned up some code

--- find_gaze.m
+++ find_gaze.m
@@ -6,9 +6,12 @@ t = linspace(0,2*pi,1000);
 for i = 1:numel(t)
 
     %% 1. field of view (square) and normal vectors along the edges
+
+    % parametrisation of square field of view emitted from square
     cam.fov_square_x(i) = tan(cam.view_angle)*cos(t(i))/max(abs(cos(t(i))),abs(sin(t(i))));
     cam.fov_square_y(i) = tan(cam.view_angle)*sin(t(i))/max(abs(cos(t(i))),abs(sin(t(i))));
     cam.fov_square_z(i) = 1;
+    % create unit vectors emitting from camera
     cam.fov(:,i) = [cam.fov_square_x(i);cam.fov_square_y(i);cam.fov_square_z(i)];
     cam.fov(:,i) = cam.fov(:,i)/norm(cam.fov(:,i));     
     cam.fov(:,i) = cam.R*cam.fov(:,i); 
@@ -17,15 +20,15 @@ for i = 1:numel(t)
     % https://en.wikipedia.org/wiki/Line%E2%80%93plane_intersection#Algebraic_form
     p0 = mirror(1).p;                       % point on plane (tilt mirror)
     n = mirror(1).R(:,3);                   % normal vector of plane (tilt mirror)
-    l0 = cam.p';                             % point on line (camera fov)    
+    l0 = cam.p';                            % point on line (camera fov)    
     l = cam.fov(:,i);                       % direction vector of line (camera fov)
     d = dot(p0-l0,n)/dot(l,n);              % line parameters at for intersecting condition
     cam.pts.proj.tilt(:,i) = l0 + l*d;
 
     %% 3. reflection:: camera_line_of_sight --> tilt_mirror_plane    
-    v = cam.fov(:,i);                       % incidecnt direction (camera fov)
+    v = cam.fov(:,i);                       % incident direction (camera fov)
     a = mirror(1).R(:,3);                   % normal vector of plane (tilt mirror)
-    c = mirror(1).p;                         % point on plane (tilt mirror)        
+    c = mirror(1).p;                        % point on plane (tilt mirror)        
     cam.vec.reflect.tilt(:,i) = v-2*a*(dot(v,a)/dot(a,a));
     cam.vec.reflect.tilt(:,i) = cam.vec.reflect.tilt(:,i)/norm(cam.vec.reflect.tilt(:,i)); 
 
@@ -33,15 +36,15 @@ for i = 1:numel(t)
     % https://en.wikipedia.org/wiki/Line%E2%80%93plane_intersection#Algebraic_form
     p0 = mirror(2).p;                       % point on plane (pan mirror)
     n = mirror(2).R(:,3);                   % normal vector of plane (pan mirror)
-    l0 = cam.pts.proj.tilt(:,i) ;            % point on line (tilt mirror projection)    
+    l0 = cam.pts.proj.tilt(:,i) ;           % point on line (tilt mirror projection)    
     l = cam.vec.reflect.tilt(:,i);          % direction vector of line (tilt mirror reflection)
     d = dot(p0-l0,n)/dot(l,n);              % line parameters at for intersecting condition
     cam.pts.proj.pan(:,i) = l0 + l*d;
 
     %% 5. reflection:: tilt_mirror_reflection --> pan_mirror_plane    
-    v = cam.vec.reflect.tilt(:,i);                       % incidecnt direction (tilt mirror reflection)
+    v = cam.vec.reflect.tilt(:,i);          % incidecnt direction (tilt mirror reflection)
     a = mirror(2).R(:,3);                   % normal vector of plane (pan mirror)
-    c = mirror(2).p;                         % point on plane (pan mirror)        
+    c = mirror(2).p;                        % point on plane (pan mirror)        
     cam.vec.reflect.pan(:,i) = v-2*a*(dot(v,a)/dot(a,a));
     cam.vec.reflect.pan(:,i) = cam.vec.reflect.pan(:,i)/norm(cam.vec.reflect.pan(:,i)); 
 
@@ -51,7 +54,7 @@ for i = 1:numel(t)
     n = [0 0 1];                           % normal vector of plane (griund)
     l0 = cam.pts.proj.pan(:,i);            % point on line (pan mirror projection)    
     l = cam.vec.reflect.pan(:,i);          % direction vector of line (pan mirror reflection)
-    d = dot(p0-l0,n)/dot(l,n);              % line parameters at for intersecting condition
+    d = dot(p0-l0,n)/dot(l,n);             % line parameters at for intersecting condition
     cam.pts.proj.ground(:,i) = l0 + l*d;
 end
 
--- galvo_gui.m
+++ galvo_gui.m
@@ -6,19 +6,20 @@ function galvo_gui
 
     %% setup geometry
     
-    mirror(1).angle  = -10;
-    mirror(2).angle  =  10;
+    mirror(1).angle  = -10;  % tilt (degree)
+    mirror(2).angle  =  10;  % pan  (degree)
     
+    % orientation, position, field of view (fov needs more work)
     cam.R = eulerzyx_fast([pi, 0, 0]);
     cam.p = [0 0 0];
-    cam.view_angle = 1.5*pi/180;
+    cam.view_angle = 1.5*pi/180; % set this to zero to see projection of point instead of square fov
     
     
-    % mirror 1
-    mirror(1).p = cam.p'  + cam.R(:,3)*10;
+    % mirror 1 -- tilt mirror position and orientation
+    mirror(1).p = cam.p'  + cam.R(:,3)*10; % can add some offset along x and y and would still work
     mirror(1).R = eulerzyx_fast([0,0,180]*pi/180)*eulerzyx_fast([-45,0,0]*pi/180)*eulerzyx_fast([ mirror(1).angle 0 0]*pi/180);
-    % mirror 2
-    mirror(2).p = mirror(1).p + cam.R(:,2)*7.25 + cam.R(:,1)*0; 
+    % mirror 2 -- pan mirror position and orientation
+    mirror(2).p = mirror(1).p + cam.R(:,2)*7.25; % again, can add some offset and would still work 
     mirror(2).R = eulerzyx_fast([0,-90,0]*pi/180)*eulerzyx_fast([-45,0,0]*pi/180)*eulerzyx_fast( [mirror(2).angle 0 0]*pi/180);
 
 
@@ -30,38 +31,44 @@ function galvo_gui
     
     ff.sp = subplot(1,2,1);
     hold on
-    s = loadawobj('galvo_minimal.obj');
-    s.v(3,:) = -s.v(3,:);
+    s = loadawobj('galvo_minimal.obj'); %  loads a minimal geometry with camera and mirror holders
+    s.v(3,:) = -s.v(3,:);               %  need these coordinate flipping alond x and z direction
     s.v(1,:) = -s.v(1,:);
     ff.galvo_minimal = patch('Faces',s.f3','Vertices',s.v','facecolor','b','edgealpha',0.1);
+
+    % ground plane patch
     ff.gnd = patch([-1 -1 1 1]*70,[-1 1 1 -1]*70, [1 1 1 1]*-28,'k');
     set(ff.gnd,'FaceAlpha',0.2,'facecolor','k');
     
+    % tilt mirror patch
     ff.mirror(1) =  patch([-1 -1 1 1]*4,[-1 1 1 -1]*4, [0 0 0 0],'k');
     set(ff.mirror(1),'FaceAlpha',0.5,'facecolor','y');
     mirror(1).vertices = ff.mirror(1).Vertices';
     ff.mirror(1).Vertices =(mirror(1).R*mirror(1).vertices + mirror(1).p)';
         
+    % pan mirror patch 
     ff.mirror(2) =  patch([-1 -1 1 1]*4,[-1 1 1 -1]*4, [0 0 0 0],'k');
     set(ff.mirror(2),'FaceAlpha',0.5,'facecolor','y');
     mirror(2).vertices = ff.mirror(2).Vertices';
     ff.mirror(2).Vertices =(mirror(2).R*mirror(2).vertices + mirror(2).p)';
 
-    % field_of_view projection on tilt mirror
+    % field_of_view (square) projection on tilt mirror
     ff.ph.proj.tilt = plot3(0,0,0,'m','LineWidth',1,'marker','o','markersize',1);
 
-    % field_of_view projection on pan mirror
+    % field_of_view (square) projection on pan mirror
     ff.ph.proj.pan  = plot3(0,0,0,'m','LineWidth',1,'marker','o','markersize',1);
 
-    % filed_of_view projection on ground
+    % filed_of_view (square) projection on ground
     ff.ph.proj.gnd  = plot3(0,0,0,'m','LineWidth',1,'marker','o','markersize',1);
     
     %% gui controls
+
     % slider for tilt angle
     ff.sl.t = uicontrol('style','slider','units','pixel','string','tilt','tag','tag_sl_tilt'); 
     ff.sl.t.Max = max(-5);
     ff.sl.t.Min = min(-15);
     addlistener(ff.sl.t,'ContinuousValueChange',@(hObject, event) update_system(hObject, event,ff,cam,mirror));
+
     % slider for pan angle
     ff.sl.p = uicontrol('style','slider','units','pixel','string','pan','tag','tag_sl_pan'); 
     ff.sl.p.Max = max(10);
@@ -77,33 +84,30 @@ function galvo_gui
         ff.st(ii) = uicontrol('style','text','units','normalized','tag',['var_' num2str(ii)]);
     end
 
+    % arrange the gui controls
     format_gui
 
+    % bring system to a good home position
     reset_system(ff,cam,mirror)
 
-    
-    
-
-%% Visualize geometry
-
-
-
-
+    %% reset callback
 function reset_system(ff,cam,mirror)
     
     subplot(ff.sp)
 
     % reset the angles
-    mirror(1).angle  = -10;
-    mirror(2).angle  =  10;
+    mirror(1).angle  = -10; % tilt
+    mirror(2).angle  =  10; % pan
 
     % reset mirror orientations
     mirror(1).R = eulerzyx_fast([0,0,180]*pi/180)*eulerzyx_fast([-45,0,0]*pi/180)*eulerzyx_fast([ mirror(1).angle 0 0]*pi/180);
     mirror(2).R = eulerzyx_fast([0,-90,0]*pi/180)*eulerzyx_fast([-45,0,0]*pi/180)*eulerzyx_fast( [mirror(2).angle 0 0]*pi/180);
 
+    % update mirror patches
     ff.mirror(1).Vertices =(mirror(1).R*mirror(1).vertices + mirror(1).p)';
     ff.mirror(2).Vertices =(mirror(2).R*mirror(2).vertices + mirror(2).p)';
 
+    % plot properties
     axis equal
     view(-75,30)
     xlim([-2.5 0.2]*30)
@@ -138,21 +142,24 @@ function reset_system(ff,cam,mirror)
     ff.ph.proj.gnd.YData = cam.pts.proj.ground(2,:);
     ff.ph.proj.gnd.ZData = cam.pts.proj.ground(3,:);
    
-
+%% update callback
 function update_system(hObject, event,ff,cam,mirror)
     
-
+    % update mirror angles from reading the slider object values
     hh = findobj('tag','tag_sl_tilt');
     mirror(1).angle = hh.Value;
     hh = findobj('tag','tag_sl_pan');
     mirror(2).angle = hh.Value;
 
+    % update mirror rotations
     mirror(1).R = eulerzyx_fast([0,0,180]*pi/180)*eulerzyx_fast([-45,0,0]*pi/180)*eulerzyx_fast([ mirror(1).angle 0 0]*pi/180);
     mirror(2).R = eulerzyx_fast([0,-90,0]*pi/180)*eulerzyx_fast([-45,0,0]*pi/180)*eulerzyx_fast( [mirror(2).angle 0 0]*pi/180);
     
+    % update mirror plot patches
     ff.mirror(1).Vertices =(mirror(1).R*mirror(1).vertices + mirror(1).p)';
     ff.mirror(2).Vertices =(mirror(2).R*mirror(2).vertices + mirror(2).p)';
 
+    % update text in plot
     gg = findobj('tag','var_1');     
     gg.String=['tilt = ' num2str(mirror(1).angle)  '°']; 
     gg = findobj('tag','var_2');     
